1ac56c6c7954fda5e9ef0a50ab3fd5b65f36cee5
Mary314 <makaovamaria@yandex.ru>
2025-08-23 18:10:11 +0300

    Add files via upload
    Реализация поиска нод двух чисел рекурсивно на языке с++.

---
+++ НОД_(рекурсия).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int f(int a, int b){
+    if(b == 0){
+        return a;
+    }
+    else{
+        return f(b, a % b);
+    }
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a > b){
+        swap(a, b);
+        cout << f(a, b);
+    }
+    else if(b == 0 && a == 0){
+        cout << "null";
+        return 0;
+    }
+    else{
+        cout << f(a, b);
+    }
+    return 0;
+}
