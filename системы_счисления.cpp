32e9d3bfd5337fb548bceac2f1cf50da64855ddb
Mary314 <makaovamaria@yandex.ru>
2025-08-31 22:10:00 +0300

    Перевод из любой с-мы в десятичную

--- системы_счисления.cpp
+++ системы_счисления.cpp
@@ -1,40 +1,21 @@
-//алгоритм перевода из десятичной с-мы в любую:
+//Перевод из любой с-мы в десятичную:
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-    int n, base;
-    cin >> n;
+    string k;
+    int base;
+    getline(cin, k);
     cin >> base;
-    string res = "";
-    while (n > 0) {
-        char a;
-        if (n % base < 10) {
-            a = 48 + (n % base);
-        }
-        if (n % base == 10) {
-            a = 'A';
-        }
-        if (n % base == 11) {
-            a = 'B';
-        }
-        if (n % base == 12) {
-            a = 'C';
-        }
-        if (n % base == 13) {
-            a = 'D';
-        }
-        if (n % base == 14) {
-            a = 'E';
-        }
-        if (n % base == 15) {
-            a = 'F';
-        }
-        //и так далее
-        res = a + res;
-        n /= base;
-        cout << res;
-        }
+    int n = 0;
+    int len = k.size();
+    for (int i = 0; i < len; i++) {
+        n += (k[i] - 48) * pow(base, len - i - 1);
+    }
+    cout << n;
+
+    return 0;
 }
